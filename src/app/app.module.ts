b51bd1bc8e0beed4033f46bfad11fdfd9961f45d
XanVranck <vranckaert.xan@gmail.com>
2018-06-04 20:58:30 +0200

    Added home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { MatToolbarModule, MatMenuModule, MatButtonToggleModule, MatIconModule } from '@angular/material';
+import { MatToolbarModule, MatMenuModule, MatButtonToggleModule, MatIconModule,
+        MatCardModule } from '@angular/material';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { AppRoutingModule } from './routing/app-routing.module';
@@ -21,6 +22,7 @@ import { HomeComponent } from './home/home.component';
     MatMenuModule,
     MatButtonToggleModule, 
     MatIconModule, 
+    MatCardModule,
     AppRoutingModule,
     RouterModule
   ],

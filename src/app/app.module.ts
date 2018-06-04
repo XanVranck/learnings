61bf34b2eb8bccbf5d0eb4f4207b63db5e922d7b
XanVranck <vranckaert.xan@gmail.com>
2018-06-04 20:17:11 +0200

    added a routerModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { MatToolbarModule, MatMenuModule, MatButtonToggleModule, MatIconModule } from '@angular/material';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { AppRoutingModule } from './routing/app-routing.module';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,7 @@ import { MenuComponent } from './menu/menu.component';
     MatToolbarModule,
     MatMenuModule,
     MatButtonToggleModule, 
-    MatIconModule
+    MatIconModule, AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
